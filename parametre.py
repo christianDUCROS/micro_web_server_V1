c052be4901fd43856688a4a5dd48317181eb0b95
christianDUCROS <96232175+christianDUCROS@users.noreply.github.com>
2023-02-15 13:49:38 +0100

    Add files via upload

---
+++ parametre.py
@@ -0,0 +1,11 @@
+#paramétrage site web    
+repertoire = 'www/'
+index_page='/index.html'
+
+#parametres wifi station ou AP
+ssid="ssid_box"
+password="password_box"
+
+#mode WIFI
+#mode_wifi = 'AP'
+mode_wifi = 'STA'
